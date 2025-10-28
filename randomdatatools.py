8232b06caf7fa383a88a7e25c0ab16db7f468b94
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-10-29 01:18:11 +0300

    refactor: remove randomdatatools.py from root (moved to src/)

--- randomdatatools.py
+++
@@ -1,7 +0,0 @@
-import requests
-class Client():
-	def __init__(self):
-		self.api="http://api.randomdatatools.ru"
-		#gender=man ,woman,unset . type=classic,rare,all . count = 1-100
-	def get_name(self,gender:str,type:str,count: str="1",unescaped: str="false"):
-		return requests.get(f"{self.api}?gender={gender}&typeName={type}&count={count}&unescaped={unescaped}").json()
